525cfa22daea907b09d2247d2e5c36785bc88a31
Kevin Kmetz <kev.kmetz@gmail.com>
2019-01-09 09:52:10 +0800

    Starting working on and testing the append methods in the Raster class.

--- TerrainGenerator/Raster.java
+++ TerrainGenerator/Raster.java
@@ -4,38 +4,85 @@ import java.util.LinkedList;
 
 public class Raster {
 
-	int width;
-	int height;
+	private int width;
+	private int height;
 
-	LinkedList<LinkedList<Integer>> raster;		// A LinkedList of rows of integers.
+	private int[][] raster;		// A LinkedList of rows of integers.
 
-	public Raster (int width, int height) {
+	public Raster(int width, int height) {
 
 		this.width = width;
 		this.height = height;
 
-		raster = new LinkedList<LinkedList<Integer>>();
+		raster = new int[height][width];
 
 		for (int y = 0; y < height; y++) {
-			raster.add(new LinkedList<Integer>());
+
+			for (int x = 0; x < width; x++) {
+
+				raster[y][x] = -1;
+
+			}
+
 		}
 
-		for (LinkedList<Integer> i : raster) {
-			for (int x = 0; x < width; x++) {
-				i.add(-1);
-			}
-		}
+	}
+
+	public void set(int x, int y, int value) {
+
+		raster[y][x] = value;
+
+	}
+
+	public int get(int x, int y) {
+
+		return raster[y][x];
 
 	}
 
 	public void display() {
 
-		for (LinkedList<Integer> i : raster) {
-			for (Integer k : i) {
-				System.out.print(k + " ");
+		for (int y = 0; y < height; y++) {
+
+			for (int x = 0; x < width; x++) {
+
+				System.out.print(raster[y][x] + " ");
+
 			}
+
 			System.out.println();
+
 		}
+
+	}
+
+	public void appendTop(int[] row) {
+
+		int[][] updatedRaster = new int[height][width];
+
+		System.arraycopy(row, 0, updatedRaster[0], 0, updatedRaster[0].length);
+
+		for (int i = 0; i < raster.length-1; i++) {
+			System.arraycopy(raster[i], 0, updatedRaster[i+1], 0, raster[i].length);
+		}
+
+		System.arraycopy(updatedRaster, 0, raster, 0, raster.length);
+
+	}
+
+	public void appendTop(int[][] rows) {
+
+		int[][] updatedRaster = new int[height][width];
+
+		for (int i = 0; i < rows.length; i++) {
+			System.arraycopy(rows[i], 0, updatedRaster[i], 0, updatedRaster[i].length);
+		}
+
+		for (int i = rows.length; i < height; i++) {
+			System.arraycopy(raster[i-rows.length], 0, updatedRaster[i], 0, updatedRaster[i].length);
+		}
+
+		System.arraycopy(updatedRaster, 0, raster, 0, raster.length);
 
 	}
 
--- TerrainGenerator/Tests/RasterTest.java
+++ TerrainGenerator/Tests/RasterTest.java
@@ -2,6 +2,8 @@ package TerrainGenerator.Tests;
 
 import TerrainGenerator.Raster;
 
+import java.util.Arrays;
+
 class RasterTest {
 
 	public static void main(String[] args) {
@@ -12,7 +14,45 @@ class RasterTest {
 
 	RasterTest() {
 
-		Raster raster = new Raster(6, 5);
+		final int width = 6;
+		final int height = 5;
+
+		Raster raster = new Raster(width, height);
+
+		// Test setting of pixels in the raster.
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				raster.set(x, y, y*width + x + 1);
+			}
+		}
+
+		raster.display();
+
+		// Test setting individual elements in the raster.
+		raster.set(1, 0, 5);
+		raster.set(3, 4, 8);
+
+		System.out.println();
+		raster.display();
+
+		// Test adding a row to the top of the raster.
+		int[] tempArray = new int[width];
+		Arrays.fill(tempArray, 99);
+		raster.appendTop(tempArray);
+
+		System.out.println();
+		raster.display();
+
+		// Test adding multiple rows to the top of the raster.
+		int[][] tempMultiArray = new int [3][width];
+
+		for (int[] i : tempMultiArray) {
+			Arrays.fill(i, 77);
+		}
+
+		raster.appendTop(tempMultiArray);
+
+		System.out.println();
 		raster.display();
 
 	}
